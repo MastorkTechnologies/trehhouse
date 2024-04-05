86207233a937e136830353721224e28954912d53
Ashutosh Kewat <109691178+Ashu6200@users.noreply.github.com>
2024-04-05 10:33:40 +0530

    mobile view fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import './Navbar.css';
+import "./Navbar.css";
 import { FaAngleDown } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   const [openBar, setOpenBar] = useState(false);
 
-  const [logostyle, setLogoStyle] = useState({width:"120%"})
+  const [logostyle, setLogoStyle] = useState({ width: "120%" });
 
   useEffect(() => {
     setWindowDimension(window.innerWidth);
@@ -19,7 +19,7 @@ const Navbar = () => {
 
   useEffect(() => {
     function handleResize() {
-      console.log(window.innerWidth)
+      console.log(window.innerWidth);
       setWindowDimension(window.innerWidth);
     }
 
@@ -27,86 +27,114 @@ const Navbar = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  const isSmallWindow = windowDimension <=1436;
+  const isSmallWindow = windowDimension <= 1436;
 
-  const isMobile = windowDimension <=1252;
+  const isMobile = windowDimension <= 1252;
 
-  useEffect(()=>{
-      console.log(isSmallWindow)
-      if(isSmallWindow){
-        setLogoStyle({width:"100%"})
-      }else{
-        setLogoStyle({width:"120%"})
-      }
-
-  },[isSmallWindow])
-
+  useEffect(() => {
+    console.log(isSmallWindow);
+    if (isSmallWindow) {
+      setLogoStyle({ width: "100%" });
+    } else {
+      setLogoStyle({ width: "120%" });
+    }
+  }, [isSmallWindow]);
 
   return (
     <>
-    {isMobile?<>
-        <ul className="Navbar" >
-        <li className="listItem" onClick={()=>setOpenBar(!openBar)}>
-        <GiHamburgerMenu />     
-        </li>
+      {isMobile ? (
+        <>
+          <ul className='Navbar'>
+            <li className='listItem' onClick={() => setOpenBar(!openBar)}>
+              <GiHamburgerMenu />
+            </li>
 
-        <li className="Logo">
-            <img src="./images/Trehouse.png" style={logostyle} />
-        </li>
-        
-        <li className="listItem purpbtnm" style={{marginLeft:"2.4vw"}}>
-        <a className='aw' href='https://calendly.com/trehouse-bd/30min?month=2024-04'>Contact Us</a>
-        </li>
-    </ul>
-    {openBar&&
-    <ul className="inlinenested">
-                <li className="listItem">
-                    <p>Home</p>
-                </li>
-                <li className="listItem">
-                    <p><a className='ab' href='#ourmodel'>Our Model</a></p>
-                </li>
-                <li className="listItem">
-                    <p><a className='ab' href='#Hww'>How we work</a></p>
-                </li>
-                <li className="listItem">
-                    {/* <p><a className='ab' href='#testimonials'>Testimonials</a></p> */}
-                </li>
-                
-            </ul>
-    }
-    </>:<>
-    <ul className="Navbar" >
-        <li className="Logo">
-            <img src="./images/Trehouse.png" style={logostyle} />
-        </li>
-        <li>
-            <ul className="nestedList">
-                <li className="listItem">
-                    <p>Home</p>
-                </li>
-                <li className="listItem">
-                    <p><a className='ab' href='#ourmodel'>Our Model</a></p>
-                </li>
-                <li className="listItem">
-                    <p><a className='ab' href='#Hww'>How we work</a></p>
-                </li>
-                <li className="listItem">
-                    {/* <p><a className='ab' href='#testimonials'>Testimonials</a></p> */}
-                </li>
-            </ul>
-        </li>
-        <li>
-            <ul className="nestedList">
-                
-                <li className="listItem purpbtn" style={{marginLeft:"2.4vw"}}>
-                <a className='aw' href='https://calendly.com/trehouse-bd/30min?month=2024-04'>Contact Us</a>
-                </li>
-            </ul>
-        </li>
-    </ul>
-    </>}
+            <li className='Logo'>
+              <img src='./images/Trehouse.png' style={logostyle} />
+            </li>
 
+            <li className='listItem purpbtnm' style={{ marginLeft: "2.4vw" }}>
+              <a
+                className='aw'
+                href='https://calendly.com/trehouse-bd/30min?month=2024-04'
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
+          {openBar && (
+            <ul className='inlinenested'>
+              <li className='listItem'>
+                <p>Home</p>
+              </li>
+              <li className='listItem'>
+                <p>
+                  <a className='ab' href='#ourmodel'>
+                    Our Model
+                  </a>
+                </p>
+              </li>
+              <li className='listItem'>
+                <p>
+                  <a className='ab' href='#Hww'>
+                    How we work
+                  </a>
+                </p>
+              </li>
+              <li className='listItem'>
+                {/* <p><a className='ab' href='#testimonials'>Testimonials</a></p> */}
+              </li>
+            </ul>
+          )}
+        </>
+      ) : (
+        <>
+          <ul className='Navbar'>
+            <li className='Logo'>
+              <img src='./images/Trehouse.png' style={logostyle} />
+            </li>
+            <li>
+              <ul className='nestedList'>
+                <li className='listItem'>
+                  <p>Home</p>
+                </li>
+                <li className='listItem'>
+                  <p>
+                    <a className='ab' href='#ourmodel'>
+                      Our Model
+                    </a>
+                  </p>
+                </li>
+                <li className='listItem'>
+                  <p>
+                    <a className='ab' href='#Hww'>
+                      How we work
+                    </a>
+                  </p>
+                </li>
+                <li className='listItem'>
+                  {/* <p><a className='ab' href='#testimonials'>Testimonials</a></p> */}
+                </li>
+              </ul>
+            </li>
+            <li>
+              <ul className='nestedList'>
+                <li
+                  className='listItem purpbtn'
+                  style={{ marginLeft: "2.4vw" }}
+                >
+                  <a
+                    className='aw'
+                    href='https://calendly.com/trehouse-bd/30min?month=2024-04'
+                  >
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </>
+      )}
     </>
   );
 };
